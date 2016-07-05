dcf9b2e2e36863103f9b5dcd7c893d8d4a6b1b6b
sibomots <sibomots@mondolab.net>
2016-07-04 23:54:11 -0700

    first commit

---
+++ src/com/sibomots/plugin/adventure/Adventure.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Adventure, licensed under the BSD 3-Clause License
+ *
+ * Copyright (c) SiboRoc <http://siboroc.net>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sibomots.plugin.adventure;
+
+
+import com.google.inject.Inject;
+import org.apache.logging.log4j.core.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameAboutToStartServerEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+
+@Plugin(id = "adventure", name = "Adventure", version = "1.0.0", description = "Adventure Time!")
+
+public class Adventure {
+
+    public static Adventure instance;
+    public static final String MOD_ID = "Adventure";
+    @Inject
+    public PluginContainer pluginContainer;
+    @Inject private Logger logger;
+
+    @Listener
+    public void onAboutToStart(GameAboutToStartServerEvent event) {
+
+        instance = this;
+        // this.loadConfig();
+        // this.customLogger = new CustomLogger();
+    }
+
+    @Listener
+    public void onServerStarted(GameStartedServerEvent event) {
+        registerBaseCommands();
+        //addLogEntry("Boot finished.");
+        this.logger.info("Loaded successfully.");
+    }
+
+    // handles sub commands
+    public void registerBaseCommands() {
+    }
+
+}
