0ec26819b4384da73afc050cf531932bdb32ab5b
sibomots <sibomots@mondolab.net>
2016-07-05 20:20:36 -0700

    added sample command for testing

---
+++ src/com/sibomots/plugin/adventure/AdventurePermissions.java
@@ -0,0 +1,7 @@
+package com.sibomots.plugin.adventure;
+
+/**
+ * Created by jwand on 7/5/2016.
+ */
+public class AdventurePermissions {
+}
---
+++ src/com/sibomots/plugin/adventure/internal/Comms.java
@@ -0,0 +1,7 @@
+package com.sibomots.plugin.adventure.internal;
+
+/**
+ * Created by jwand on 7/5/2016.
+ */
+public class Comms {
+}
